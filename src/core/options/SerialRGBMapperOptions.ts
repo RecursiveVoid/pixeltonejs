50694fb56780e74be2d830753db764a6a7b31e60
Ergin Turk <ergin@synaps.com>
2024-11-28 13:49:27 +0100

    Add SerialRGBMapperOptions

---
+++ src/core/options/SerialRGBMapperOptions.ts
@@ -0,0 +1,10 @@
+import { RGBFrequencyRange } from "../types/RGBFrequencyRange";
+
+interface SerialRGBMapperOptions {
+  data: Uint8ClampedArray,
+  width: number,
+  height: number,
+  rgbFrequencyRange: RGBFrequencyRange,
+}
+
+export { SerialRGBMapperOptions };
